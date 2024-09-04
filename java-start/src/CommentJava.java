0b25d42094a2b68c1247c82d39b1f0bae529f78b
chwit <chwit97@gmail.com>
2024-09-04 12:25:47 +0900

    java start

---
+++ java-start/src/CommentJava.java
@@ -0,0 +1,12 @@
+public class CommentJava {
+    public static void main(String[] args) {
+        //psvm : public static void main(String[] args) 자동완성
+        System.out.println("hello java1");
+        //sout + enter -> System.out.println();
+        /*
+        System.out.println("hello java2");
+        System.out.println("hello java3");
+        */
+
+    }
+}
