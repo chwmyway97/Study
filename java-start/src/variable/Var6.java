0ff6f209dc6f40e2af07c7a61bffef771556fcbf
chojoit <chojoit@gmail.com>
2024-09-04 17:53:04 +0900

    섹션 3 -변수

--- java-start/src/variable/Var6.java
+++ java-start/src/variable/Var6.java
@@ -2,7 +2,7 @@ package variable;
 
 public class Var6 {
     public static void main(String[] args) {
-        int a  ;
+        int a  = 0 ;
         System.out.println(a);
         //수정내용 작성 완료
     }
---
+++ java-start/src/variable/ex/VarEx3.java
@@ -0,0 +1,11 @@
+package variable.ex;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long variable = 10000000000L;
+        boolean Bool = true;
+        System.out.println(variable);
+        System.out.println(Bool);
+
+    }
+}
