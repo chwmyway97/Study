649250b8ec3fde4fe3c5cf1e3471f873e81beb87
chwmyway <chwmyway@gmail.com>
2024-09-12 06:56:03 +0900

    # 반복문

--- java-start/src/loop/ex/NestedEx2.java
+++ java-start/src/loop/ex/NestedEx2.java
@@ -2,14 +2,13 @@ package loop.ex;
 
 public class NestedEx2 {
     public static void main(String[] args) {
-        int rows = 2;
+        int rows = 5;
 
-        for (int i = 0; i < rows; i++) {
-            System.out.print("*");
-            for (int j = 0; j < i; j++) {
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
-
+            System.out.println();
 
         }
     }
