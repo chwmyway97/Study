7a46a517ab6e26562f5849d8818daf12b6eb3fab
chwmyway <chwmyway@gmail.com>
2024-09-26 23:54:48 +0900

    # 스캐너

---
+++ java-start/src/scanner/ScannerWhile2.java
@@ -0,0 +1,36 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class ScannerWhile2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("첫 번째 숫자와 두 번째 숫자 모두 0을 입력하면 프로그램을 종료합니다.");
+        while (true) {
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int num1 = scanner.nextInt();
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int num2 = scanner.nextInt();
+
+
+            if (num1 == 0 && num2 == 0) {
+                System.out.println("프로그램 종료");
+                break;
+            }
+
+            int result = num1 + num2;
+            System.out.println("result = " + result);
+            /*처음에는
+            if (num1 == 0 && num2 == 0) {
+                System.out.println("프로그램 종료");
+                break;
+            }else{
+                int result = num1 + num2;
+                System.out.println("result = " + result);
+            이렇게 작성했었는데
+            브레이크 하는 순간 와일문은 끝이기 때문에 굳이 엘스문을 해줄 필요는 없겠다.
+            */
+        }
+    }
+}
