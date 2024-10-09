c5186f5fc456bb4ad22365ad75d5292b812a1649
chwmyway <chwmyway@gmail.com>
2024-10-09 21:19:22 +0900

    # 배열

---
+++ java-start/src/array/ex/ArrayEx7.java
@@ -0,0 +1,48 @@
+package array.ex;
+
+import java.util.Scanner;
+
+public class ArrayEx7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //학생수 입력
+        int students = 4;
+
+        //각 과목명에 해당하는 문자열 배열
+        String[] subjects = {"국어", "영어", "수학"};
+
+        //4명의 학생, 각 학생당 3개의 성적을 저장할 배열
+        int[][] scores = new int[students][subjects.length];
+
+        //학생별 성적 입력
+        for (int i = 0; i < scores.length; i++) {
+            System.out.println((i + 1) + "번의 학생의 성적을 입력하세요:");
+            for (int j = 0; j < scores[i].length; j++) {
+                System.out.print(subjects[j] + " 점수:");
+                scores[i][j] = scanner.nextInt();
+            }
+        }
+
+        // 학생별 총점 및 평균 계산 후 출력
+        for (int i = 0; i < scores.length; i++) {
+            int total = 0;
+            double average = 0;
+
+            // 각 학생의 성적을 합산
+            for (int j = 0; j < scores[i].length; j++) {
+                total += scores[i][j];
+            }
+
+            //평균 계산
+            average = (double) total / scores[i].length;
+
+            // 총점 및 평균 출력
+            System.out.println((i + 1) + "번 학생의 총점: " + total + ", 평균: " + average);
+
+
+        }
+
+
+    }
+}
---
+++ java-start/src/array/ex/ProductAdminEx.java
@@ -0,0 +1,58 @@
+package array.ex;
+
+import java.util.Scanner;
+
+public class ProductAdminEx {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        //상품 갯수 지정
+        int maxProducts = 10;
+        String[] productNames = new String[maxProducts];
+        int[] productPrices = new int[maxProducts];
+        int productCount = 0;
+
+        while (true) {
+            System.out.print("1. 상품 등록 | 2. 상품 목록 | 3. 종료\n메뉴를 선택하세요:");
+
+            int menuNumber = scanner.nextInt();
+            scanner.nextLine(); // /n삭제
+
+            //1.상품등록
+            if (menuNumber == 1) {
+
+                if (productCount == maxProducts) {
+                    System.out.println("더 이상 상품을 등록할 수 없습니다.");
+                    continue;
+                }
+                System.out.print("상품 이름을 입력하세요:");
+                productNames[productCount] = scanner.nextLine();
+
+                System.out.print("상품 가격을 입력하세요:");
+                productPrices[productCount] = scanner.nextInt();
+
+                productCount++;
+
+
+            } else if (menuNumber == 2) {  //2.
+
+                if (productCount == 0) {
+                    System.out.println("등록된 상품이 없습니다.");
+                    continue;
+                }
+
+                for (int i = 0; i < productCount; i++) {
+                    System.out.println(productNames[i] + ": " + productPrices[i]);
+                }
+
+            } else {  //3.
+                System.out.println("프로그램 종료");
+                break;
+            }
+
+
+        }
+    }
+
+}
